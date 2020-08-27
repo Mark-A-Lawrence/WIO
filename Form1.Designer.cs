2a8e9c1236ed0de35219b7b0bafbbf9cb78d3d0d
Webtech1234567890 <antorie.law@gmail.com>
2020-08-27 16:23:19 -0700

    project commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.srcbox = new System.Windows.Forms.TextBox();
+            this.dextexbox = new System.Windows.Forms.TextBox();
+            this.pretext = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.but = new System.Windows.Forms.Button();
+            this.prev = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lab = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.files1 = new System.Windows.Forms.CheckedListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(498, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(581, 63);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(272, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // srcbox
+            // 
+            this.srcbox.Enabled = false;
+            this.srcbox.Location = new System.Drawing.Point(581, 188);
+            this.srcbox.Name = "srcbox";
+            this.srcbox.Size = new System.Drawing.Size(254, 20);
+            this.srcbox.TabIndex = 2;
+            // 
+            // dextexbox
+            // 
+            this.dextexbox.Enabled = false;
+            this.dextexbox.Location = new System.Drawing.Point(581, 243);
+            this.dextexbox.Name = "dextexbox";
+            this.dextexbox.Size = new System.Drawing.Size(254, 20);
+            this.dextexbox.TabIndex = 3;
+            // 
+            // pretext
+            // 
+            this.pretext.Location = new System.Drawing.Point(581, 129);
+            this.pretext.Name = "pretext";
+            this.pretext.Size = new System.Drawing.Size(313, 20);
+            this.pretext.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(842, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(842, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(645, 420);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(155, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "set preview image manually";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // but
+            // 
+            this.but.Location = new System.Drawing.Point(842, 465);
+            this.but.Name = "but";
+            this.but.Size = new System.Drawing.Size(75, 23);
+            this.but.TabIndex = 11;
+            this.but.Text = "Browse";
+            this.but.UseVisualStyleBackColor = true;
+            this.but.Visible = false;
+            this.but.Click += new System.EventHandler(this.But_Click);
+            // 
+            // prev
+            // 
+            this.prev.Enabled = false;
+            this.prev.Location = new System.Drawing.Point(581, 467);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(254, 20);
+            this.prev.TabIndex = 10;
+            this.prev.Visible = false;
+            this.prev.TextChanged += new System.EventHandler(this.Prev_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(808, 503);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 42);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Compress";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 535);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(497, 10);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(704, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Quality";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "prefix (Optional)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Source Folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Destination Folder";
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(694, 451);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(64, 13);
+            this.lab.TabIndex = 18;
+            this.lab.Text = "Preview File";
+            this.lab.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(244, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Preview";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(859, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "label7";
+            // 
+            // files1
+            // 
+            this.files1.FormattingEnabled = true;
+            this.files1.Location = new System.Drawing.Point(581, 306);
+            this.files1.Name = "files1";
+            this.files1.Size = new System.Drawing.Size(336, 94);
+            this.files1.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(699, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Exceptions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 578);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.files1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.but);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pretext);
+            this.Controls.Add(this.dextexbox);
+            this.Controls.Add(this.srcbox);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Web image Optimizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox srcbox;
+        private System.Windows.Forms.TextBox dextexbox;
+        private System.Windows.Forms.TextBox pretext;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button but;
+        private System.Windows.Forms.TextBox prev;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckedListBox files1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Linq;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label7.Text = "100%";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 100;
+            trackBar1.TickStyle = TickStyle.BottomRight;
+            trackBar1.TickFrequency = 10;
+            trackBar1.Value = 100;
+           
+
+
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            label7.Text = trackBar1.Value.ToString() + "%";
+            try
+            {
+                bitm(trackBar1.Value);
+            }catch(Exception r)
+            {
+
+            }
+
+            
+
+
+        }
+
+
+        private Bitmap bitm(int quality)
+        {
+            string[] files;
+
+            if (checkBox1.Checked)
+            {
+
+                files = Directory.GetFiles(srcbox.Text);
+                files[0] = prev.Text;
+            }
+            else
+            {
+                files = Directory.GetFiles(srcbox.Text);
+            }
+
+            Bitmap b = null;
+           
+
+
+            string ext = Path.GetExtension(files[0]).ToUpper();
+            if (ext == ".PNG" || ext == ".JPG")
+            {
+
+                var FileName = Path.GetFileName(files[0]);
+
+
+
+                using (Bitmap bmp1 = new Bitmap(files[0]))
+                {
+                    ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                    System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+                    EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                    EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
+
+                    myEncoderParameters.Param[0] = myEncoderParameter;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                       
+                        bmp1.Save(ms, jpgEncoder, myEncoderParameters);
+                        Bitmap resized = new Bitmap(bmp1, new Size(pictureBox1.Width,pictureBox1.Height));
+                        pictureBox1.Image = resized;
+
+                    }
+                }
+            }
+            return b;
+
+        }
+
+
+
+        private void OpenFolderDialog(TextBox Filepath)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Filepath.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFolderDialog(srcbox);
+
+                string[] files = Directory.GetFiles(srcbox.Text);
+                ArrayList myArrayList = new ArrayList();
+
+                foreach (var item in files)
+                {
+                    myArrayList.Add(Path.GetFileName(item));
+                }
+                files1.DataSource = myArrayList;
+            }catch(Exception r)
+            {
+
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFolderDialog(dextexbox);
+        }
+
+
+        public static void CompressImage(string SoucePath, string DestPath, int quality, String prefix)
+        {
+            var FileName = Path.GetFileName(SoucePath);
+            
+
+            DestPath = DestPath + "\\" +prefix+FileName;
+
+          
+
+            using (Bitmap bmp1 = new Bitmap(SoucePath))
+            {
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
+
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                bmp1.Save(DestPath, jpgEncoder, myEncoderParameters);
+
+            }
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+            string[] files = Directory.GetFiles(srcbox.Text);
+            int amount = files.Length+1;
+            int count = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = amount;
+            string[] excfiles = new string[files1.CheckedItems.Count + 1];
+
+
+            // Determine if there are any items checked.  
+            if (files1.CheckedItems.Count != 0)
+            {
+                // If so, loop through all checked items and print results.  
+                string s = "";
+                for (int x = 0; x < files1.CheckedItems.Count; x++)
+                {
+                    excfiles[x] = srcbox.Text+@"\"+files1.CheckedItems[x].ToString();
+                }
+
+            }
+
+
+            foreach (var file in files)
+            {
+                string ext = Path.GetExtension(file).ToUpper();
+                if (ext == ".PNG" || ext == ".JPG"&& !excfiles.Contains(file))
+                    CompressImage(file, dextexbox.Text,trackBar1.Value, pretext.Text.ToString());
+                count++;
+
+                progressBar1.Value = count+1;
+            }
+
+
+
+            MessageBox.Show("Compressed Images has been stored to\n" + dextexbox.Text);
+            dextexbox.Text = "";
+            srcbox.Text = "";
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                lab.Visible = true;
+                but.Visible = true;
+                prev.Visible = true;
+            }
+            else
+            {
+                lab.Visible = false;
+                but.Visible = false;
+                prev.Visible = false;
+            }
+        }
+
+        private void But_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = srcbox.Text;
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                prev.Text = fdlg.FileName;
+            }
+        }
+
+        private void Prev_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
